45f8cb030981bcf3b70ea1607526a13ac40ee2cb
brad reynolds <reynolds.0345@gmail.com>
2016-03-03 16:55:30 -0500

    Built an example with PUT that could be manipulated to do validation.

--- src/main/java/com/EIMA/services/TimeService.java
+++ src/main/java/com/EIMA/services/TimeService.java
@@ -1,20 +1,39 @@
 package com.EIMA.services;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.EIMA.models.Time;
+import org.codehaus.jettison.json.JSONException;
 
-@Path("/time")
+import javax.ws.rs.PathParam;
+import java.util.TimeZone;
+
+import com.EIMA.models.Time;
+import com.EIMA.models.Validate;
+
+@Path("/EIMA")
 @Produces(MediaType.APPLICATION_JSON)
 public class TimeService {
 
-    @GET
-    public Time get() {
-        return new Time();
-    }
+	@GET
+	public Time get() {
+		return new Time();
+	}
 
+	@GET
+	@Path("/{timezone}")
+	public Time get(@PathParam("timezone") String timezone) {
+		return new Time(TimeZone.getTimeZone(timezone.toUpperCase()));
+	}
+
+	@PUT
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Path("/validate")
+	public Validate putExample(String json) throws JSONException {				
+		return new Validate(json);
+	}
 }
-
