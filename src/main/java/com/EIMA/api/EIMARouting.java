fbd3ffc524f145d95ca4a52b87511861a1856703
Brad Reynolds <reynolds.0345@gmail.com>
2016-03-16 16:07:28 -0400

    Switched to auto inject JSON.
    Put Roberts notes in a text file to work from. and cleaned up the code
    to start implementing raw code outlines

--- src/main/java/com/EIMA/api/EIMARouting.java
+++ src/main/java/com/EIMA/api/EIMARouting.java
@@ -1,13 +1,12 @@
 package com.EIMA.api;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 import com.EIMA.outputs.AccessResult;
 import com.EIMA.outputs.CreateIncidentResult;
@@ -16,236 +15,150 @@ import com.EIMA.outputs.ResultBase;
 import com.EIMA.outputs.UserListResult;
 import com.EIMA.outputs.UserProfileResult;
 
-import javax.ws.rs.PathParam;
-import java.util.TimeZone;
-
 @Path("/EIMA")
 @Produces(MediaType.APPLICATION_JSON)
 public class EIMARouting {
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/auth/login")
-	public ResultBase login(String username, String password) {
-		// validate the username and password
-		// if valid {
-		String token = "random token";
-		// store the token in database
+	public ResultBase login(JSONObject json) {
+
 		return null;
-		// }
-		// else {
-		// return false;
-		// }
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/auth/logout")
-	public ResultBase logout(String token) {
-		// if token is in database:
-		// remove token from database
+	public ResultBase logout(JSONObject json) {
 		return null;
-		// if token is not in database:
-		// return false;
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/auth/validatetoken")
-	public ResultBase validateToken(String token) {
-		// validate the token
-		// if valid:
+	public ResultBase validateToken(JSONObject json) {
 		return null;
-		// if invalid:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/auth/validateprivilege")
-	public ResultBase validatePrivilege(String token, String incidentID, String requiredPrivLevel) {
-		// validate the privilege of the user
-		// if valid:
+	public ResultBase validatePrivilege(JSONObject json) {
 		return null;
-		// if invalid:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/incidentaccess/join")
-	public ResultBase joinIncident(String token, String incidentID) {
-		// validate the token and incident ID
-		// if valid:
-		// join the user to the incident
+	public ResultBase joinIncident(JSONObject json) {
 		return null;
-		// if invalid:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/incidentaccess/create")
-	public CreateIncidentResult create(String token) {
-		// validate the token
-		// if valid:
-		// create a new incident id, set user level to admin, store in
-		// database
+	public CreateIncidentResult create(JSONObject json) {
 		return null;
-		// if invalid:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/incidentaccess/createstandalone")
-	public CreateIncidentResult createStandalone(String token, String incidentData) {
-		// validate the token
-		// if valid:
-		// create a new incident from standalone data
+	public CreateIncidentResult createStandalone(JSONObject json) {
 		return null;
-		// if invalid:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/incidentaccess/leave")
-	public ResultBase leave(String token) {
-		// validate the token
-		// if valid:
-		// validate privilege
-		// if valid:
-		// add asset info to database
-		// remove user from the incident
+	public ResultBase leave(JSONObject json) {
 		return null;
-		// if invalid token or privilege:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/users/getprofile")
-	public UserProfileResult getProfile(String token) {
-		// validate the token
-		// if valid:
-		// display the profile information
+	public UserProfileResult getProfile(JSONObject json) {
 		return null;
-		// if invalid:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/users/setprofile")
-	public ResultBase setProfile(String token, String profileData) {
-		// validate the token
-		// if valid:
-		// store profileData in database
+	public ResultBase setProfile(JSONObject json) {
 		return null;
-		// if invalid:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/users/registeruser")
-	public ResultBase registerUser(String username, String password, String profileData) {
-		// store new user and profileData in database
+	public ResultBase registerUser(JSONObject json) {
 		return null;
-		// if error:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/incidentdata/mapdata")
-	public MapDataResult getMapData(String token) {
-		// validate the token
-		// if valid:
-		// display the map data
+	public MapDataResult getMapData(JSONObject json) {
 		return null;
-		// if invalid:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/incidentdata/accesslevel")
-	public AccessResult getAccessLevel(String token) {
-		// validate the token
-		// if valid:
-		// display the access level of user
+	public AccessResult getAccessLevel(JSONObject json) {
 		return null;
-		// if invalid:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/incidentdata/location")
-	public ResultBase location(String token, double Latitude, double Longitute) {
-		// validate the token
-		// if valid:
-		// push the location data
+	public ResultBase location(JSONObject json) {
 		return null;
-		// if invalid:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/mapedit/addassetinfo")
-	public ResultBase addAssetInfo(String token, String mapData) {
-		// validate the token
-		// if valid:
-		// validate privilege
-		// if valid:
-		// add asset info to database
+	public ResultBase addAssetInfo(JSONObject json) {
 		return null;
-		// if invalid privilege or token:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/admin/userlist")
-	public UserListResult userList(String Token) {
-		// validate the token
-		// if valid:
-		// validate privilege
-		// if valid:
-		// display list of users and all info associated with them
+	public UserListResult userList(JSONObject json) {
 		return null;
-		// if invalid token or privilege:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/admin/sendmessage")
-	public ResultBase sendMessage(String Token, String username, String message) {
-		// validate the token
-		// if valid:
-		// validate privilege
-		// if valid:
-		// send a message to the user
+	public ResultBase sendMessage(JSONObject json) {
 		return null;
-		// if invalid token or privilege:
-		// return false;
+
 	}
 
 	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/admin/editprivilege")
-	public ResultBase editPrivilege(String Token, String username, String privilegeLevel) {
-		// validate the token
-		// if valid:
-		// validate privilege
-		// if valid:
-		// change the users privilege level
+	public ResultBase editPrivilege(JSONObject json) {
 		return null;
-		// if invalid token or privilege level:
-		// return false;
+
 	}
 }
