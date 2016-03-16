762a25a592d444531df72761d131ba17e22276c0
brad reynolds <reynolds.0345@gmail.com>
2016-03-16 14:03:15 -0400

    start of auth utils

--- src/main/java/com/EIM/auth/AuthUtils.java
+++ src/main/java/com/EIM/auth/AuthUtils.java
@@ -1,26 +1,76 @@
 package com.EIM.auth;
 
+import java.util.concurrent.TimeUnit;
+
+import com.EIMA.Database.DBQueries;
+
+import java.util.UUID;
+
 public class AuthUtils {
 
 	public static enum Privlege {
 		NO_ACCESS, USER, MAP_EDITOR, ADMIN
 	}
 
-	public static boolean login() {
+	public static boolean login(String uname, String pwd) {
+		return DBQueries.validateLogin(uname, pwd);
+	}
+
+	public static boolean hasPrivlege(String token, Privlege requiredPriv) {
+		Privlege usersP = getUserPrivleges(token);
+
+		// Admin
+		if (requiredPriv.equals(Privlege.ADMIN)) {
+			return usersP.equals(Privlege.ADMIN);
+		}
+		// MapEditor
+		if (requiredPriv.equals(Privlege.MAP_EDITOR)) {
+			return usersP.equals(Privlege.ADMIN) || usersP.equals(Privlege.MAP_EDITOR);
+		}
+		// User
+		if (requiredPriv.equals(Privlege.MAP_EDITOR)) {
+			return usersP.equals(Privlege.ADMIN) || usersP.equals(Privlege.MAP_EDITOR) || usersP.equals(Privlege.USER);
+		}
+		// NoAccess
 		return true;
 	}
 
-	public static boolean hasPrivlege(String token, Privlege p) {
-		return true;
+	public static Privlege getUserPrivleges(String token) {
+		String priv = usersCurrentPrivLevel(token);
+
+		if (priv == "Admin") {
+			return Privlege.ADMIN;
+		} else if (priv == "Map Editor") {
+			return Privlege.MAP_EDITOR;
+		} else if (priv == "User") {
+			return Privlege.USER;
+		}
+		return Privlege.NO_ACCESS;
 	}
 
 	public static String generateToken() {
+		long a = TimeUnit.NANOSECONDS.toMicros(System.nanoTime());
+		String uuid = UUID.randomUUID().toString();
+
+		StringBuilder token = new StringBuilder();
+
+		token.append(convertHex(a));
+		token.append(uuid);
+
+		return token.toString();
+	}
+
+	public static String usersCurrentPrivLevel(String token) {
+		// would call a DB function to get users current incident
 		return null;
 	}
-	
+
 	public static int usersCurrentIncident(String token) {
-		//would call a DB function to get users current incident
+		// would call a DB function to get users current incident
 		return 1;
 	}
-	
+
+	private static String convertHex(long n) {
+		return Long.toHexString(n);
+	}
 }
--- src/main/java/com/EIMA/Database/DBQueries.java
+++ src/main/java/com/EIMA/Database/DBQueries.java
@@ -4,4 +4,20 @@ package com.EIMA.Database;
 
 public class DBQueries {
 
+	/**
+	 * Takes username/password. returns a true false value. This SHOULD use a
+	 * Prepared statement to Prevent SQL injection. This is important. Other
+	 * queries don't require this as non of their input makes it to the actual
+	 * SQL statement exception of token validation (I think).
+	 * 
+	 * http://www.theserverside.com/news/1365244/Why-Prepared-Statements-are-
+	 * important-and-how-to-use-them-properly
+	 * http://stackoverflow.com/questions/24692296/how-to-use-prepared-statement
+	 * -for-select-query-in-java
+	 * http://www.java2novice.com/jdbc/prepared-statement/
+	 */
+	public static boolean validateLogin(String uname, String pwd) {
+		return false;
+	}
+
 }
