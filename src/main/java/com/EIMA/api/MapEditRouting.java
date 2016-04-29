5ff6093dc2e939cb44ac3487b888c405be6a6fb0
brad reynolds <reynolds.0345@gmail.com>
2016-04-29 00:56:15 -0400

    start of parse work

--- src/main/java/com/EIMA/api/MapEditRouting.java
+++ src/main/java/com/EIMA/api/MapEditRouting.java
@@ -11,6 +11,9 @@ import org.codehaus.jettison.json.JSONObject;
 
 import com.EIMA.Database.DBQueries;
 import com.EIMA.auth.AuthUtils;
+import com.EIMA.models.EIMAAsset;
+import com.EIMA.models.EIMACircle;
+import com.EIMA.models.EIMAPolygon;
 import com.EIMA.outputs.*;
 
 @Consumes(MediaType.APPLICATION_JSON)
@@ -25,10 +28,16 @@ public class MapEditRouting {
 
 			if (DBQueries.tokenIsValid(token) && DBQueries.userIsInIncident(token)
 					&& AuthUtils.hasPrivlege(token, AuthUtils.Privlege.mapEditor)) {
-
-				DBQueries.updateMapAsset(null, token);
-				DBQueries.deleteMapAsset(null, token);
-				DBQueries.addMapAsset(null, token);
+				
+				EIMAAsset[] assets = DBQueries.getAssets(token);
+				EIMACircle[] circ = DBQueries.getCircles(token);
+				EIMAPolygon[] poly = DBQueries.getPolygons(token);
+				//PARSE MAGIC GOES HERE
+				
+//				DBQueries.updateMapAsset(null, token);
+//				DBQueries.deleteMapAsset(null, token);
+//				DBQueries.addMapAsset(null, token);
+				
 				return new ResultBase(true);
 
 			}
